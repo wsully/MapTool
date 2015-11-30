9a4cb73e467a4b984b9aeec3c61a3a3663aadf0f
Lumbini Parnas <lparnas@wpi.edu>
2015-11-30 14:49:51 -0500

    Node Name contains map name

--- GetThere/src/DevGUI.java
+++ GetThere/src/DevGUI.java
@@ -517,11 +517,16 @@ public class DevGUI extends JPanel{
 			g2d.setColor(Color.BLACK);
 
 			for (int i = 0; i < currentStartNodes.size(); i++){
-				((Graphics2D)g).draw(new Rectangle (currentStartNodes.get(i).getX()-SquareWidth/2, currentStartNodes.get(i).getY()-SquareWidth/2, SquareWidth, SquareWidth));
+				((Graphics2D)g).draw(new Rectangle (currentStartNodes.get(i).getX()-SquareWidth/2, 
+													currentStartNodes.get(i).getY()-SquareWidth/2,
+													SquareWidth, SquareWidth));
 			}
 
 			for (int i = 0; i < currentStartEdges.size(); i++){
-				((Graphics2D)g).draw(new Line2D.Double(currentStartEdges.get(i).getNode1().getX(), currentStartEdges.get(i).getNode1().getY(),currentStartEdges.get(i).getNode2().getX(),currentStartEdges.get(i).getNode2().getY() ));
+				((Graphics2D)g).draw(new Line2D.Double(currentStartEdges.get(i).getNode1().getX(), 
+														currentStartEdges.get(i).getNode1().getY(),
+														currentStartEdges.get(i).getNode2().getX(),
+														currentStartEdges.get(i).getNode2().getY() ));
 			}
 		}
 
